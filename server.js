51515055eb4c93288aef0dd5e50094c2bc023d00
shadowmoonezios <agushifelde@hotmail.com>
2025-11-05 13:36:35 +0200

    Updated mongoose connect options for better compatibility.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: false })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
